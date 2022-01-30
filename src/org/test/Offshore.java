1a57998444b8802f1058189e709468d2f6a19289
Radhakrishnanjrk <radhakrishnan.j@outlook.in>
2022-01-30 17:06:07 +0530

    password functionality

--- src/org/test/Offshore.java
+++ src/org/test/Offshore.java
@@ -2,4 +2,14 @@ package org.test;
 
 public class Offshore {
 
+	public static void password() {
+		
+       System.out.println("password");
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		password();
+	}
 }
