910d3943cc3a0ef8fdea31bfee9fd5f3c42dbc49
Radhakrishnanjrk <radhakrishnan.j@outlook.in>
2022-01-30 16:57:10 +0530

    username functionality

--- src/org/test/Offshore.java
+++ src/org/test/Offshore.java
@@ -1,5 +1,19 @@
 package org.test;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+
 public class Offshore {
+	
+	public static void username() {
+		
+     System.out.println("UserName");
+     	
+	}
+	public static void main(String[] args) {
+		
+		username();
+		
+		
+	}
 
 }
